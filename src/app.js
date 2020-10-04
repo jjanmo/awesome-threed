e1784c45af9513b1cd205480da9745023920af62
jjanmo <jjanmo07@hanmail.net>
2020-10-04 23:20:59 +0900

    Update image animation

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 	const $texts = document.querySelectorAll('.step');
 
 	let $currentImage = document.querySelector('.visible');
+	let actionObj;
 
 	function setIndex() {
 		$images.forEach((image, index) => {
@@ -14,16 +15,23 @@
 	function showImage(index) {
 		$currentImage = $images[index];
 		$currentImage.classList.add('visible');
+		if ($currentImage.dataset.action) {
+			const key = $currentImage.dataset.action;
+			actionObj[key](true);
+		}
 	}
 
 	function hideImage() {
 		$currentImage.classList.remove('visible');
+		if ($currentImage.dataset.action) {
+			const key = $currentImage.dataset.action;
+			actionObj[key](false);
+		}
 	}
 
 	function handleScroll() {
-		$texts.forEach((text, index) => {
+		$texts.forEach((text) => {
 			const textPosY = text.getBoundingClientRect().top;
-			console.log(textPosY);
 			const currentIndex = text.dataset.index;
 			if (textPosY > window.innerHeight * 0.1 && textPosY < window.innerHeight * 0.8) {
 				hideImage();
@@ -35,6 +43,19 @@
 	function init() {
 		setIndex();
 		window.addEventListener('scroll', handleScroll);
+
+		actionObj = {
+			bird: document.querySelector('[data-index="2"] .bird'),
+			bird2: document.querySelector('[data-index="5"] .bird'),
+			flyStraight(flag) {
+				if (flag) this.bird.style.transform = 'translateX(110vw)';
+				else this.bird.style.transform = 'translateX(-100%)';
+			},
+			flyDiagonally(flag) {
+				if (flag) this.bird2.style.transform = `translate(${window.innerWidth}px, -${window.innerHeight * 0.8}px)`;
+				else this.bird2.style.transform = '';
+			},
+		};
 	}
 
 	init();
