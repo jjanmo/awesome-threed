8f814201f717c74667e89ac4192fdccfc2fe75ef
jjanmo <jjanmo07@hanmail.net>
2020-10-05 02:37:44 +0900

    Update scroll performance using intersection observer and set scroll initialization

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 (function () {
 	const $images = document.querySelectorAll('.graphic-item');
 	const $texts = document.querySelectorAll('.step');
-
+	const io = new IntersectionObserver(ioCallback);
 	let $currentImage;
 	let actionObj;
+	let ioIndex;
+
+	function ioCallback(entries, _) {
+		const { target } = entries[0];
+		ioIndex = target.dataset.index;
+	}
 
 	function setIndex() {
 		$images.forEach((image, index) => {
@@ -12,38 +18,57 @@
 		});
 	}
 
+	function setObserver() {
+		$texts.forEach((text) => io.observe(text));
+	}
+
 	function showImage(index) {
 		$currentImage = $images[index];
 		$currentImage.classList.add('visible');
 		if ($currentImage.dataset.action) {
-			const key = $currentImage.dataset.action;
-			actionObj[key](true);
+			callAction(true);
 		}
 	}
 
 	function hideImage() {
 		$currentImage.classList.remove('visible');
 		if ($currentImage.dataset.action) {
-			const key = $currentImage.dataset.action;
-			actionObj[key](false);
+			callAction(false);
 		}
 	}
 
+	function callAction(flag) {
+		const key = $currentImage.dataset.action;
+		actionObj[key](flag);
+	}
+
 	function handleScroll() {
-		$texts.forEach((text) => {
-			const textPosY = text.getBoundingClientRect().top;
-			const currentIndex = text.dataset.index;
+		for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
+			if (!$texts[i]) continue;
+
+			const textPosY = $texts[i].getBoundingClientRect().top;
+			const currentIndex = $texts[i].dataset.index;
 			if (textPosY > window.innerHeight * 0.1 && textPosY < window.innerHeight * 0.8) {
 				hideImage();
 				showImage(currentIndex);
 			}
-		});
+		}
 	}
 
 	function init() {
 		setIndex();
+		setObserver();
 		showImage(0);
+
 		window.addEventListener('scroll', handleScroll);
+
+		//스크롤에 따른 이미지 변화를 새로고침시 마다 초기화해주기 위한 작업
+		//scrollTo()가 바로 안먹는 경우가 있기 때문에 setTimeout을 사용한 것
+		window.addEventListener('load', () => {
+			setTimeout(() => {
+				window.scrollTo(0, 0);
+			}, 100);
+		});
 
 		actionObj = {
 			bird: document.querySelector('[data-index="2"] .bird'),
