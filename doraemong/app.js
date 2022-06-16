2897da7f16ce64ae4dae88fae2fbf368510353fc
jjanmo <jjanmo07@hanmail.net>
2022-06-16 23:17:50 +0900

    refactor: refactor to seperate functions

--- doraemong/app.js
+++ doraemong/app.js
@@ -1,26 +1,26 @@
-const $leftEye = document.querySelector('.left');
-const $rightEye = document.querySelector('.right');
+const $pupils = document.querySelectorAll('.pupil'); // 눈동자들
 
-function moveEyes(target, mouse) {
-  const $pupil = target.querySelector('.pupil'); // 눈동자
-  const pupilArea = $pupil.getBoundingClientRect();
+function getDegree(target, mouse) {
+  const pupilArea = target.getBoundingClientRect(); //  눈동자 영역에 대한 정보
   const { x, y, width, height } = pupilArea;
-  const centerX = x + width * 0.5;
-  const centerY = y + height * 0.5;
-
+  const centerX = x + width * 0.5; // 영역의 X 중심좌표
+  const centerY = y + height * 0.5; // 영역의 Y 중심좌표
   const degree =
-    (Math.atan2(mouse.y - centerY, mouse.x - centerX) * 180) / Math.PI;
-  $pupil.style.transform = `rotate(${degree + 90}deg)`; // + 90도 : 최초 위치 보정
+    (Math.atan2(mouse.y - centerY, mouse.x - centerX) * 180) / Math.PI; // 눈동가자 움직이는 각도
+
+  return degree;
+}
+
+function moveEye(target, mouse) {
+  const degree = getDegree(target, mouse);
+  target.style.transform = `rotate(${degree + 90}deg)`; // + 90도 : 최초 위치 보정
 }
 
 window.addEventListener('mousemove', (e) => {
-  const posX = e.clientX;
-  const posY = e.clientY;
   const mouse = {
-    x: posX,
-    y: posY,
+    x: e.clientX,
+    y: e.clientY,
   };
 
-  moveEyes($leftEye, mouse);
-  moveEyes($rightEye, mouse);
+  $pupils.forEach(($pupil) => moveEye($pupil, mouse));
 });
