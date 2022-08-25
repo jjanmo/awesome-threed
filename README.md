74e79a505ce0a1da0b66a0fd2940f3a557ae154e
jjanmo <jjanmo07@hanmail.net>
2022-08-25 12:44:55 +0900

    feat: setup next project and change readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Awesome Canvas
+# Awesome ThreeD
 
-> 캔버스를 활용한 재미있는 것들 만들기!
+> Three.js를 활용한 다양한 예시들을 만들어보는 프로젝트 with Next.js
--- src/shooter/app.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.querySelector('#canvas');
-const context = canvas.getContext('2d');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const player = new Player(canvas.width / 2, canvas.height / 2, 50, 'tomato');
-player.draw();
