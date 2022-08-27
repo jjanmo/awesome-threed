277ca7f0cf7acc65af1e5828f3d153278c073ad9
jjanmo <jjanmo07@hanmail.net>
2022-08-27 19:20:54 +0900

    feat: add reset style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import ResetStyle from '../styles/ResetStyle'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps}>
+      <ResetStyle />
+    </Component>
+  )
 }
 
 export default MyApp
